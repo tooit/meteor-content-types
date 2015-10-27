fbe16e9e0094506e0c2346295db8e0a85389cd89
Julian Montagna <julian.montagna@tooit.com>
2015-10-27 02:55:14 -0300

    updated package version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'tooit:content-types',
   summary: 'A Meteor package to easly define application document types.',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'git@github.com:tooit/meteor-content-types.git'
 });
 
